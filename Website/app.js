40b936e64a16a3baf3bfc06236fd4a95477a7e4c
TheDoctorTTV <thedoctor@thetimevortex.net>
2026-07-19 13:54:33 -0400

    Fix package template indentation in Website app

--- Website/app.js
+++ Website/app.js
@@ -3,31 +3,30 @@ import { baseLayerLuminance, StandardLuminance } from 'https://unpkg.com/@fluent
 const LISTING_URL = "{{ listingInfo.Url }}";
 
 const PACKAGES = {
-{ { ~ for package in packages ~} }
-"{{ package.Name }}": {
-  name: "{{ package.Name }}",
+{{~ for package in packages ~}}
+  "{{ package.Name }}": {
+    name: "{{ package.Name }}",
     displayName: "{{ if package.DisplayName; package.DisplayName; end; }}",
-      description: "{{ if package.Description; package.Description; end; }}",
-        version: "{{ package.Version }}",
-          author: {
-    name: "{{ if package.Author.Name; package.Author.Name; end; }}",
+    description: "{{ if package.Description; package.Description; end; }}",
+    version: "{{ package.Version }}",
+    author: {
+      name: "{{ if package.Author.Name; package.Author.Name; end; }}",
       url: "{{ if package.Author.Url; package.Author.Url; end; }}",
     },
-  dependencies: {
-    { { ~ for dependency in package.Dependencies ~} }
-    "{{ dependency.Name }}": "{{ dependency.Version }}",
-      {{ ~end ~}
-  }
-},
-keywords: [
-  {{~ for keyword in package.Keywords ~}}
-"{{ keyword }}",
-  {{ ~end ~}}
+    dependencies: {
+      {{~ for dependency in package.Dependencies ~}}
+        "{{ dependency.Name }}": "{{ dependency.Version }}",
+      {{~ end ~}}
+    },
+    keywords: [
+      {{~ for keyword in package.Keywords ~}}
+        "{{ keyword }}",
+      {{~ end ~}}
     ],
-license: "{{ package.License }}",
-  licensesUrl: "{{ package.LicensesUrl }}",
+    license: "{{ package.License }}",
+    licensesUrl: "{{ package.LicensesUrl }}",
   },
-{ { ~end ~} }
+{{~ end ~}}
 };
 
 const setTheme = () => {
